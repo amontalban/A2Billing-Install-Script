eca79643a0b065cfdcc1985231a5bd0f6cd2ccf4
Andres Montalban <amontalban@gmail.com>
2012-09-21 12:40:53 -0300

    Update A2Billing package link

--- a2billing_setup.sh
+++ a2billing_setup.sh
@@ -316,7 +316,7 @@ netstat -nlt | grep :80 >> $LOG_FILE 2>&1
 displayResult $?
 
 displayMessage "Downloading A2Billing script version $A2BILLING_VERSION"
-wget -t 3 --no-check-certificate https://github.com/Star2Billing/a2billing/tarball/$A2BILLING_VERSION -O $WORK_DIRECTORY/A2Billing_$A2BILLING_VERSION.tar.gz >> $LOG_FILE 2>&1
+wget -t 3 --no-check-certificate https://raw.github.com/amontalban/A2Billing-Install-Script/master/A2Billing_$A2BILLING_VERSION.tar.gz -O $WORK_DIRECTORY/A2Billing_$A2BILLING_VERSION.tar.gz >> $LOG_FILE 2>&1
 displayResult $?
 
 displayMessage "Extracting A2Billing script in $WORK_DIRECTORY"
@@ -867,7 +867,7 @@ cd /usr/src >> $LOG_FILE 2>&1
 displayResult $?
 
 displayMessage "Downloading A2Billing patch ..."
-wget -t 3 --no-check-certificate https://raw.github.com/amontalban/A2Billing-Install-Script/master/a2billing.patch
+wget -t 3 --no-check-certificate https://raw.github.com/amontalban/A2Billing-Install-Script/master/a2billing.patch >> $LOG_FILE 2>&1
 displayResult $?
 
 displayMessage "Applying A2Billing patches ..."
